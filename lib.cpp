128d78e82746607fa7cefdda62a09eb68d6587ae
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-05-02 16:17:55 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float Array (float num[], int d){
+    float max=0;
+    for (int i=0 ; i<d; i++){
+        if (max < num[i]){
+            max=num[i];
+        }
+    }
+    return max;
+}
