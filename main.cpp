233de6905aa78b82d43bb3a431edcfaed7c85746
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-05-02 16:18:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
 
 int main(){
+  int dimensione;
+    cin >> dimensione;
+    float numeri[dimensione];
+
+    for(int i=0 ; i<dimensione; i++) {
+        cin >> numeri[i];
+    }
+
+    float r = Array(numeri, dimensione);
+    cout << r << endl;
   return 0;
 }
